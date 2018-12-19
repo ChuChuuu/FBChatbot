2ed287d751b72679201ea45d3d567788a820e1ae
Chu <zhichu@1997@gmail.com>
2018-12-19 18:26:25 +0800

    fix the tab problem

--- fsm.py
+++ fsm.py
@@ -87,7 +87,7 @@ class TocMachine(GraphMachine):
         print("I'm entering about")
 
         sender_id = event['sender']['id']
-		send_image_message(sender_id,"https://i.imgur.com/RqwhJzY.jpg")
+        send_image_message(sender_id,"https://i.imgur.com/RqwhJzY.jpg")
         send_text_message(sender_id, "我就是一隻鴨肉飯")
         send_three_template(sender_id,"不過你可以試著問我","帶我去你的粉專吧","想吃東西嗎","幫我想我等等要吃什麼")
         self.go_back()    
